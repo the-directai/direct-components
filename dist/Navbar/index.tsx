66b16912d60cd732a1bdf14f4e041f30798fdf66
Michal Chruscielski <michalch775@gmail.com>
2023-10-06 13:04:44 +0200

    test 1

---
+++ dist/Navbar/index.tsx
@@ -0,0 +1,9 @@
+import {FunctionComponentElement, ReactElement} from "react";
+
+function Navbar(): FunctionComponentElement<ReactElement> {
+    return (
+        <div>test</div>
+    );
+}
+
+export default Navbar;
