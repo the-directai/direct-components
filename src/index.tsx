a949bc7a061c1b0b37e945191d31e4a54a7a6b7d
Michal Chruscielski <michalch775@gmail.com>
2023-10-09 15:58:13 +0200

    hot fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {ErrorView} from "./lib";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ErrorView } from "./lib";
 
-
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
     <React.StrictMode>
         <ErrorView />
-    </React.StrictMode>
+    </React.StrictMode>,
 );
