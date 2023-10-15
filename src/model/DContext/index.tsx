36832e4f4138d33ba83ce25db758538826c5f8ee
Michal Chruscielski <michalch775@gmail.com>
2023-10-15 22:42:16 +0200

    FED-12-context-setup

---
+++ src/model/DContext/index.tsx
@@ -0,0 +1,37 @@
+/**
+ * Direct Components
+ *
+ * Copyright (c) DirectAI.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.md file in the root directory of this source tree.
+ *
+ * @version 0.1
+ * @license MIT
+ * @author Michał Chruścielski <michalch775@gmail.com>
+ */
+
+import "./styles-context.scss";
+import React from "react";
+
+type DContextProps = {
+    darkMode: boolean;
+    children?: React.JSX.Element[] | React.JSX.Element | string;
+};
+
+/**
+ * Context to manage components in library. It manages styles and will manage notifications and data flow in the future.
+ * @version 0.1
+ * @param {DContextProps} props
+ * @param {boolean} props.darkMode dark mode switch
+ * @param {React.JSX.Element[] | React.JSX.Element | string } [props.children] app content
+ */
+function DContext({ darkMode = true, children }: DContextProps) {
+    return (
+        <div className={`direct--context ${darkMode ? "d--dark--mode" : "d--light--mode"}`}>
+            {children ? children : <></>}
+        </div>
+    );
+}
+
+export default DContext;
