dfa7dbc442d8b25ae76c3771a223aecab172828f
Michal Chruscielski <michalch775@gmail.com>
2023-10-20 15:23:04 +0200

    v0.1.20-beta.0

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,23 +2,14 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import scss from "rollup-plugin-scss";
 import packageJson from "./package.json" assert { type: 'json' };
-import styles from "rollup-plugin-styles";
 import postcss from 'rollup-plugin-postcss';
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import image from '@rollup/plugin-image';
-import font from "rollup-plugin-font";
 import autoprefixer from "autoprefixer";
-import url from '@rollup/plugin-url';
-import css from "rollup-plugin-css-only";
 import postcssImport from "postcss-import";
-import postcssUrl from "postcss-url";
 import cssnano from "cssnano";
-import postcssPartialImport from "postcss-partial-import";
-import stylelint from "stylelint";
 import postcssFontMagician from "postcss-font-magician";
-import sourcemaps from "rollup-plugin-sourcemaps";
 
 
 export default [
@@ -45,11 +36,12 @@ export default [
                     postcssImport(),
                     postcssFontMagician(),
                     autoprefixer(),
-                    //cssnano(),
+                    cssnano(),
                 ],
-                extract: true,
+                extract: false,
                 autoModules: true,
-                sourceMap: true
+                sourceMap: true,
+                minimize: true
             }),
             resolve(),
             commonjs(),
