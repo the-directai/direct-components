1bc2a6aded1a6083de7b88b9dc1ee713c859fa74
Tomek <tomek@el-jot.eu>
2023-10-28 18:20:27 +0200

    storybook fixing in process

--- src/components/ErrorView/index.tsx
+++ src/components/ErrorView/index.tsx
@@ -1,5 +1,15 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
 function ErrorView() {
-    return <main> </main>;
+    return(
+        <div className={styles.holder}>
+            <main>
+                <h1>wd</h1>
+            </main>
+        </div>
+    );
+
 }
 
 export default ErrorView;
