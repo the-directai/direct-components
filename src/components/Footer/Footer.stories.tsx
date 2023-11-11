dfd605c27a376059f6c9068d9fd2bd102e513ab3
Tomek <tomek@el-jot.eu>
2023-11-11 19:36:40 +0100

    working on footer, for now i have array of footer content

---
+++ src/components/Footer/Footer.stories.tsx
@@ -0,0 +1,17 @@
+import Footer from "./index"
+
+export default {
+    component: Footer,
+    title: 'Footer',
+    tags: ['autodocs'],
+  };
+
+export const Default = {
+    args: {
+      task: {
+        id: '2',
+        title: 'Test Footer',
+        state: 'TASK_INBOX',
+      },
+    },
+  };
