61bf7eb839b43a29471b822f15ebc618c71e93a5
Tomek <tomek@el-jot.eu>
2023-11-18 21:27:59 +0100

    router errors

--- src/components/Footer/Footer.stories.tsx
+++ src/components/Footer/Footer.stories.tsx
@@ -15,60 +15,65 @@ export default meta;
 type Story = StoryObj<typeof Footer>;
 
 export const Default: Story = (args: object) => (
-        <Router navigator={Footer} location={Footer}>
-            <Footer {...args} >
-                <FooterMain>
-                    <FooterColumn>
-                        <FooterSection>
-                            <FooterSectionHeader link={"https://mylink.com"}>
-                                Example section
-                            </FooterSectionHeader>
-                            <FooterSectionElement link={"https://mylink.com"}>
-                                Link 1
-                            </FooterSectionElement>
-                            <FooterSectionElement link={"https://mylink.com"}>
-                                Link 2
-                            </FooterSectionElement>
-                            <FooterSectionElement link={"https://mylink.com"}>
-                                Link 3
-                            </FooterSectionElement>
-                        </FooterSection>
-                    </FooterColumn>
-                    <FooterColumn>
-                        <FooterSection>
-                            <FooterSectionHeader link={"https://mylink.com"}>
-                                Example section 2
-                            </FooterSectionHeader>
-                            <FooterSectionElement link={"https://mylink.com"}>
-                                Link 1
-                            </FooterSectionElement>
-                            <FooterSectionElement link={"https://mylink.com"}>
-                                Link 2
-                            </FooterSectionElement>
-                            <FooterSectionElement link={"https://mylink.com"}>
-                                Link 3
-                            </FooterSectionElement>
-                        </FooterSection>
-                    </FooterColumn>
-                </FooterMain>
-                <FooterBottom>
-                    <FooterAdditionalText link={"https://mylink.com"}>
-                        Text us or sth
-                    </FooterAdditionalText>
-                    <FooterCaption>
-                        <FooterCaptionElement link={"https://mylink.com"}>
-                            Copyright
-                        </FooterCaptionElement>
-                        <FooterCaptionElement link={"https://mylink.com"}>
-                            Cool link
-                        </FooterCaptionElement>
-                        <FooterCaptionElement link={"https://mylink.com"}>
-                            Cool link 2
-                        </FooterCaptionElement>
-                    </FooterCaption>
-                </FooterBottom>
-            </Footer>
-        </Router>
+        <Router>
+            <Routes>
+                <Route path="/" element= {
+                    <Footer {...args} >
+                        <FooterMain>
+                            <FooterColumn>
+                                <FooterSection>
+                                    <FooterSectionHeader link={"https://mylink.com"}>
+                                        Example section
+                                    </FooterSectionHeader>
+                                    <FooterSectionElement link={"https://mylink.com"}>
+                                        Link 1
+                                    </FooterSectionElement>
+                                    <FooterSectionElement link={"https://mylink.com"}>
+                                        Link 2
+                                    </FooterSectionElement>
+                                    <FooterSectionElement link={"https://mylink.com"}>
+                                        Link 3
+                                    </FooterSectionElement>
+                                </FooterSection>
+                            </FooterColumn>
+                            <FooterColumn>
+                                <FooterSection>
+                                    <FooterSectionHeader link={"https://mylink.com"}>
+                                        Example section 2
+                                    </FooterSectionHeader>
+                                    <FooterSectionElement link={"https://mylink.com"}>
+                                        Link 1
+                                    </FooterSectionElement>
+                                    <FooterSectionElement link={"https://mylink.com"}>
+                                        Link 2
+                                    </FooterSectionElement>
+                                    <FooterSectionElement link={"https://mylink.com"}>
+                                        Link 3
+                                    </FooterSectionElement>
+                                </FooterSection>
+                            </FooterColumn>
+                        </FooterMain>
+                        <FooterBottom>
+                            <FooterAdditionalText link={"https://mylink.com"}>
+                                Text us or sth
+                            </FooterAdditionalText>
+                            <FooterCaption>
+                                <FooterCaptionElement link={"https://mylink.com"}>
+                                    Copyright
+                                </FooterCaptionElement>
+                                <FooterCaptionElement link={"https://mylink.com"}>
+                                    Cool link
+                                </FooterCaptionElement>
+                                <FooterCaptionElement link={"https://mylink.com"}>
+                                    Cool link 2
+                                </FooterCaptionElement>
+                            </FooterCaption>
+                        </FooterBottom>
+                    </Footer>
+                    </Router>
+                } />
+            </Routes>
+
 
 );
 
