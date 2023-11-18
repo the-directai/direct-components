9f853f6a5f4316e5dab6bb10a2606648b93d8dfc
Tomek <tomek@el-jot.eu>
2023-11-18 22:08:18 +0100

    Finished outer layer - text prints but not styled, stylees will be added soon

--- src/components/Footer/Footer.stories.tsx
+++ src/components/Footer/Footer.stories.tsx
@@ -1,6 +1,6 @@
 import {Meta, StoryObj} from "@storybook/react";
 import Footer, {FooterSection, FooterSectionHeader, FooterSectionElement, FooterColumn, FooterMain, FooterBottom, FooterAdditionalText, FooterCaption, FooterCaptionElement} from "./index";
-import {Route, Router, Routes} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 
 const meta: Meta<typeof Footer> = {
@@ -14,64 +14,60 @@ export default meta;
 type Story = StoryObj<typeof Footer>;
 
 export const Default: Story = (args: object) => (
-        <Router location={undefined} navigator={undefined}>
-            <Routes>
-                <Route path="/" element= {
-                    <Footer {...args} >
-                        <FooterMain>
-                            <FooterColumn>
-                                <FooterSection>
-                                    <FooterSectionHeader NavLink={"https://myNavLink.com"}>
-                                        Example section
-                                    </FooterSectionHeader>
-                                    <FooterSectionElement NavLink={"https://myNavLink.com"}>
-                                        NavLink 1
-                                    </FooterSectionElement>
-                                    <FooterSectionElement NavLink={"https://myNavLink.com"}>
-                                        NavLink 2
-                                    </FooterSectionElement>
-                                    <FooterSectionElement NavLink={"https://myNavLink.com"}>
-                                        NavLink 3
-                                    </FooterSectionElement>
-                                </FooterSection>
-                            </FooterColumn>
-                            <FooterColumn>
-                                <FooterSection>
-                                    <FooterSectionHeader NavLink={"https://myNavLink.com"}>
-                                        Example section 2
-                                    </FooterSectionHeader>
-                                    <FooterSectionElement NavLink={"https://myNavLink.com"}>
-                                        NavLink 1
-                                    </FooterSectionElement>
-                                    <FooterSectionElement NavLink={"https://myNavLink.com"}>
-                                        NavLink 2
-                                    </FooterSectionElement>
-                                    <FooterSectionElement NavLink={"https://myNavLink.com"}>
-                                        NavLink 3
-                                    </FooterSectionElement>
-                                </FooterSection>
-                            </FooterColumn>
-                        </FooterMain>
-                        <FooterBottom>
-                            <FooterAdditionalText NavLink={"https://myNavLink.com"}>
-                                Text us or sth
-                            </FooterAdditionalText>
-                            <FooterCaption>
-                                <FooterCaptionElement NavLink={"https://myNavLink.com"}>
-                                    Copyright
-                                </FooterCaptionElement>
-                                <FooterCaptionElement NavLink={"https://myNavLink.com"}>
-                                    Cool NavLink
-                                </FooterCaptionElement>
-                                <FooterCaptionElement NavLink={"https://myNavLink.com"}>
-                                    Cool NavLink 2
-                                </FooterCaptionElement>
-                            </FooterCaption>
-                        </FooterBottom>
-                    </Footer>
-                } />
-            </Routes>
-        </Router>
+        <BrowserRouter>
+            <Footer {...args} >
+                <FooterMain>
+                    <FooterColumn>
+                        <FooterSection>
+                            <FooterSectionHeader link={"https://mylink.com"}>
+                                Example section
+                            </FooterSectionHeader>
+                            <FooterSectionElement link={"https://mylink.com"}>
+                                link 1
+                            </FooterSectionElement>
+                            <FooterSectionElement link={"https://mylink.com"}>
+                                link 2
+                            </FooterSectionElement>
+                            <FooterSectionElement link={"https://mylink.com"}>
+                                link 3
+                            </FooterSectionElement>
+                        </FooterSection>
+                    </FooterColumn>
+                    <FooterColumn>
+                        <FooterSection>
+                            <FooterSectionHeader link={"https://mylink.com"}>
+                                Example section 2
+                            </FooterSectionHeader>
+                            <FooterSectionElement link={"https://mylink.com"}>
+                                link 1
+                            </FooterSectionElement>
+                            <FooterSectionElement link={"https://mylink.com"}>
+                                link 2
+                            </FooterSectionElement>
+                            <FooterSectionElement link={"https://mylink.com"}>
+                                link 3
+                            </FooterSectionElement>
+                        </FooterSection>
+                    </FooterColumn>
+                </FooterMain>
+                <FooterBottom>
+                    <FooterAdditionalText link={"https://mylink.com"}>
+                        Text us or sth
+                    </FooterAdditionalText>
+                    <FooterCaption>
+                        <FooterCaptionElement link={"https://mylink.com"}>
+                            Copyright
+                        </FooterCaptionElement>
+                        <FooterCaptionElement link={"https://mylink.com"}>
+                            Cool link
+                        </FooterCaptionElement>
+                        <FooterCaptionElement link={"https://mylink.com"}>
+                            Cool link 2
+                        </FooterCaptionElement>
+                    </FooterCaption>
+                </FooterBottom>
+            </Footer>
+        </BrowserRouter>
 
 );
 
